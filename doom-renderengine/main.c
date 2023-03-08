888d403978bfbd287fd488ceb5848ced9cea5f03
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-03-07 20:39:38 -0600

    Initial Commit via GitAuto

--- doom-renderengine/main.c
+++ doom-renderengine/main.c
@@ -6,6 +6,9 @@
 //
 
 #include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#define GL_SILENCE_DEPRECATION
 
 int main(int argc, const char * argv[]) {
     // insert code here...
---
+++ doom-renderengine/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  doom-renderengine
+//
+//  Created by Juan Pablo Gutierrez on 07/03/23.
+//
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#define GL_SILENCE_DEPRECATION
+
+int main(void)
+{
+    //Init GLFW
+    glfwInit();
+
+    //Setup glfw window settings
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* Window = glfwCreateWindow(800, 600, "macOS OpenGL engine", nullptr, nullptr);
+
+    glfwMakeContextCurrent(Window);
+    
+    //init GLEW
+    glewInit();
+    
+    glViewport(0, 0, 800, 600);
+    
+    std::cout << "GL version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GL vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "GL renderer: " << glGetString(GL_RENDERER) << std::endl;
+    
+    //Game loop
+    while(!glfwWindowShouldClose(Window))
+    {
+        glClearColor(0.2f, 0.65f, 0.154f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(Window);
+        
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+}
